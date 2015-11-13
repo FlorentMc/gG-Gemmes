d5f9bdc4d288c521f99a0a210fcc1eacd067f134
AntoineMonserand <antoine.monserand@sciencespo.fr>
2015-11-13 18:42:48 +0100

    TwoSect_GoodwinKeenInf.m
    j'ai réarrangé l'ordre des équations pour coller avec la future version avec règle de Taylor, enlevé la ligne save parameters.mat et corrigé l'expression de l'inflation (il manquait la division par le "dt" de la dérivation de p_1 et p_2)

--- GoodKeen plus Inflation 2 sectors/TwoSect_GoodwinKeenInf_system.m
+++ GoodKeen plus Inflation 2 sectors/TwoSect_GoodwinKeenInf_system.m
@@ -1,6 +1,25 @@
 function [ dX ] = TwoSect_GoodwinKeenInf_system( t,X )
 
-load parameters.mat;
+nu_1 = 4;
+nu_2 = 4;
+alpha = 0.025;
+beta = 0.02;
+delta_1 = 0.01;
+delta_2 = 0.01;
+theta_1 = 0.5;
+theta_2 = 0.5;
+a_11 = 0.01;
+a_12 = 0.01;
+a_21 = 0.01;
+a_22 = 0.01;
+phi0 = 0.04/(1-0.04^2);
+phi1 = 0.04^3/(1-0.04^2);
+eta_1 = 0.1;
+eta_2 = 0.1;
+gamma = 0.8;
+zeta_1 = 0.1;
+zeta_2 = 0.9;
+r = 0.03;
 
 K_1 = X(1);
 K_2 = X(2);
@@ -18,28 +37,28 @@ Y_1 = Q_1 - a_11*Q_1 - a_12*Q_2;
 Y_2 = Q_2 - a_21*Q_1 - a_22*Q_2;
 L_1 = al*Q_1;
 L_2 = al*Q_2;
+
+r_1 = (p_1*Q_1 - p_1*a_11*Q_1 - p_2*a_21*Q_1 - w*L_1)/(p_1*K_1);
+r_2 = (p_2*Q_2 - p_2*a_22*Q_2 - p_1*a_12*Q_2 - w*L_2)/(p_1*K_2);
+rbar = mean([r_1 r_2]);
+d_p_1 = eta_1*p_1*nu_1*(rbar-r_1);
+d_p_2 = eta_2*p_1*nu_2*(rbar-r_2);
+CPI = zeta_1*p_1 + zeta_2*p_2;
+inf = (zeta_1*d_p_1 + zeta_2*d_p_2)/CPI;
+
 Pi_1 = p_1*Q_1 - p_1*a_11*Q_1 - p_2*a_21*Q_1 - w*L_1 - r*D_1;
 Pi_2 = p_2*Q_2 - p_2*a_22*Q_2 - p_1*a_12*Q_2 - w*L_2 - r*D_2;
-r_1 = (Pi_1 + r*D_1)/(p_1*K_1);
-r_2 = (Pi_2 + r*D_2)/(p_1*K_2);
-rbar = mean([r_1 r_2]);
-CPI = zeta_1*p_1 + zeta_2*p_2;
-
 pi = (Pi_1+Pi_2) / (p_1*Y_1 + p_2*Y_2);
 f_pi = -0.0065 + exp(-5)*exp(20*pi);
+
 lambda = al/N*(Q_1+Q_2);
 phi_lambda = -phi0 + phi1/(1-lambda)^2;
 
 d_K_1 = theta_1*f_pi*Y_1 - delta_1*K_1;
 d_K_2 = theta_2*f_pi*Y_1 - delta_2*K_2;
-d_p_1 = eta_1*p_1*nu_1*(rbar-r_1);
-d_p_2 = eta_2*p_1*nu_2*(rbar-r_2);
-
-inf = (zeta_1*d_p_1 + zeta_2*d_p_2)/CPI;
-
-d_w = (phi_lambda + gammainf*inf) * w;
-d_al = - alphap * al;
-d_N = betap * N;
+d_w = (phi_lambda + gamma*inf) * w;
+d_al = - alpha * al;
+d_N = beta * N;
 d_D_1 = theta_1*f_pi*Y_1 - Pi_1;
 d_D_2 = theta_2*f_pi*Y_1 - Pi_2;
 
